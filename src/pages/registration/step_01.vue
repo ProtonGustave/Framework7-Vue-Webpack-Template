e68debcaca45e1782065a3bb7e9035f3ad85b472
Oleksandr Zhuravlov <rapidexact@gmail.com>
2017-12-01 20:02:42 +0200

    v

--- src/pages/registration/step_01.vue
+++ src/pages/registration/step_01.vue
@@ -12,7 +12,7 @@
         <div class="content signup" inner>
             <h1 class="title">Enter your age</h1>
             <div class="center-content">
-                <f7-input type="number" pattern="\d*" id="age" placeholder="Your age" @focus="handlerFocus()"/>
+                <f7-input type="number" pattern="\d*" id="age" :autofocus="true" placeholder="Your age" @focus="handlerFocus()"/>
             </div>
             <div class="bottom">
                 <p class="privacy-licence center-content">
@@ -29,7 +29,7 @@
         methods: {
             handlerFocus: function () {
                 this.$data.$$('div.bottom').css('border-bottom','35vh solid transparent');
-                this.$data.$$('#age').focus();
+//                this.$data.$$('#age').focus();
 
                 console.log(this.$data.$$);
             },
